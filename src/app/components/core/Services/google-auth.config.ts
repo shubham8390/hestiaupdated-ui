390975e0bf5800fd77587bc79854d8d96e248492
shubham390-liquilogic <shubham.shrivastav@liquilogic.in>
2025-09-27 21:27:39 +0530

    Updated string

--- src/app/components/core/Services/google-auth.config.ts
+++ src/app/components/core/Services/google-auth.config.ts
@@ -1,6 +1,7 @@
 export const googleAuthConfig = {
   client_id: '1088701936894-ak5rgjsrb09401agqfdm8p88bai9lfpd.apps.googleusercontent.com',
-  redirect_uri: 'http://localhost:4200/callback',
+  // redirect_uri: 'http://localhost:4200/callback',
+  redirect_uri: 'https://barnacle-noble-nominally.ngrok-free.app/callback',
   scope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
   auth_uri: 'https://accounts.google.com/o/oauth2/v2/auth',
   token_uri: 'https://oauth2.googleapis.com/token'
